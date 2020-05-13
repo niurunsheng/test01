7ce85da43f08af9192435a94c956d303c17f532f
niurs <1578859346@qq.com>
2020-05-13 14:23:23 +0800

    vscode冲突1

--- src/main/java/com/example/demo02/as.java
+++ src/main/java/com/example/demo02/as.java
@@ -4,5 +4,6 @@ public class as {
     public static void main(String[] args) {
         System.out.println("122");
         System.out.println("3333333333333333333333333333");
+        System.out.println("我是vscod的冲突1");
     }
 }
